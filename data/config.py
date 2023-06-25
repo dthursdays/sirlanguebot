005b6fea4827ad35d0f8b15e86175d0dce55d2f1
Никита <dthursdays@vk.com>
2023-06-25 22:06:49 +0400

    Heroku deploy

--- data/config.py
+++ data/config.py
@@ -8,11 +8,12 @@ DEBUG = 10
 
 API_TOKEN = str(os.environ.get("API_TOKEN"))
 GPT_TOKEN = str(os.environ.get("GPT_TOKEN"))
-IP = str(os.getenv('IP'))
-POSTGRES_USER = str(os.getenv('POSTGRES_USER'))
-POSTGRES_PASSWORD = str(os.getenv('POSTGRES_PASSWORD'))
-DATABASE = str(os.getenv('DATABASE'))
-POSTGRES_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{IP}/{DATABASE}'
+# IP = str(os.getenv('IP'))
+# POSTGRES_USER = str(os.getenv('POSTGRES_USER'))
+# POSTGRES_PASSWORD = str(os.getenv('POSTGRES_PASSWORD'))
+# DATABASE = str(os.getenv('DATABASE'))
+# POSTGRES_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{IP}/{DATABASE}'
+POSTGRES_URL = str(os.getenv('DATABASE_URL'))
 
 ADMINS = [
     int(os.getenv('MY_TELEGRAM_ID'))
